d5ce6e61d7b0bcfb58f1d2f7d675ce22aa60a327
ameliedesgagnes <46691459+ameliedesgagnes@users.noreply.github.com>
2021-03-07 16:16:38 -0500

    Create Initialisation.sql
    Inclusion de tous les livrables pour le 23 mars

---
+++ Initialisation.sql
@@ -0,0 +1,32 @@
+/* Nom du serveur: VMEDUBDS04\INF4163_1_2021 */
+/* Se fichier est à remettre pour le 23 mars */
+
+/* Création base de données */
+CREATE DATABASE SGH
+
+/* Utiliser la base de donnée SGH pour les prochaines commandes */
+USE SGH
+
+  /* Création des tables */
+  CREATE TABLE Client()
+  CREATE TABLE Reservation()
+  CREATE TABLE Chambre()
+  CREATE TABLE Employe()
+  CREATE TABLE Facture()
+  CREATE TABLE Hotel()
+  
+  /* Enregistrements dans les tables. Les tables doivent être chargées d'une centaine d'enregistrements */
+  
+  
+  /* Création de l'index */
+  CREATE INDEX ()
+  
+  /* Création des vues */
+  CREATE VIEW ()
+  
+  /* Altération d'une table */
+  ALTER TABLE ()
+  
+  /* Faire une prise d'écran de la représentation graphique du schéma relationnel */
+  
+  
